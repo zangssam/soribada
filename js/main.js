184b1e2b1e5aaf4602f093d32a69428db6f8a768
zangssam <zang.t0919@gmail.com>
2021-12-15 15:22:53 +0900

    자바 업데이트

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  var swiper01 = new Swiper(".mySwiper01", {
+    slidesPerView: 2.2,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+  });
+  var swiper02 = new Swiper(".mySwiper02", {});
+  var swiper03 = new Swiper(".mySwiper03", {
+    slidesPerView: 1.1,
+    spaceBetween: 10
+  });
+  var swiper04 = new Swiper(".mySwiper04", {});
+  var swiper06 = new Swiper(".mySwiper06", {
+    slidesPerView: 2,
+    spaceBetween: 10
+  });
+
+});
